ea0f90748d74ad881dcdca67701f535de349b263
yevgen-gumma <203040eg@gmail.com>
2023-12-23 17:08:49 +0200

    1

--- js/task-1.js
+++ js/task-1.js
@@ -1,12 +1,30 @@
-function isEnoughCapacity(products, containerSize) {
-  let allProducts = 0;
-  let arrayProducts = Object.values(products);
-  for (const oneProduct of arrayProducts) {
-    allProducts += oneProduct;
-  }
-  return allProducts <= containerSize ? true : false;
-}
-console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8));
-console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12));
-console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14));
-console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7));
+const customer = {
+  username: 'Mango',
+  balance: 24000,
+  discount: 0.1,
+  orders: ['Burger', 'Pizza', 'Salad'],
+  // Change code below this line
+  getBalance() {
+    return this.balance;
+  },
+  getDiscount() {
+    return this.discount;
+  },
+  setDiscount(value) {
+    this.discount = value;
+  },
+  getOrders() {
+    return this.orders;
+  },
+  addOrder(cost, order) {
+    this.balance -= cost - cost * this.discount;
+    this.orders.push(order);
+  },
+  // Change code above this line
+};
+
+customer.setDiscount(0.15);
+console.log(customer.getDiscount()); // 0.15
+customer.addOrder(5000, 'Steak');
+console.log(customer.getBalance()); // 19750
+console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
--- js/task-2.js
+++ js/task-2.js
@@ -1,33 +1,23 @@
-function calcAverageCalories(days) {
-  if (days.length === 0) {
-    return 0;
+class Storage {
+  constructor(items) {
+    this.items = items;
   }
-  let totalCalories = 0;
-  for (const oneDay of days) {
-    totalCalories += oneDay.calories;
+
+  getItems() {
+    return this.items;
   }
-  return totalCalories / days.length;
+
+  addItem(newItem) {
+    this.items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    this.items = this.items.filter(item => item !== itemToRemove);
+  }
 }
-console.log(
-  calcAverageCalories([
-    { day: 'monday', calories: 3010 },
-    { day: 'tuesday', calories: 3200 },
-    { day: 'wednesday', calories: 3120 },
-    { day: 'thursday', calories: 2900 },
-    { day: 'friday', calories: 3450 },
-    { day: 'saturday', calories: 3280 },
-    { day: 'sunday', calories: 3300 },
-  ]),
-);
-console.log(
-  calcAverageCalories([
-    { day: 'monday', calories: 2040 },
-    { day: 'tuesday', calories: 2270 },
-    { day: 'wednesday', calories: 2420 },
-    { day: 'thursday', calories: 1900 },
-    { day: 'friday', calories: 2370 },
-    { day: 'saturday', calories: 2280 },
-    { day: 'sunday', calories: 2610 },
-  ]),
-);
-console.log(calcAverageCalories([]));
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem('Droid');
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
